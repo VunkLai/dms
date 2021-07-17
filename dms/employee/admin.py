4b69653dfcb54175ad35f0f409f4ad862d3d5fc2
Vunk.Lai <vunk.lai@gmail.com>
2021-07-18 00:04:56 +0800

    [enhance] Create a admin page for Employee model

--- dms/employee/admin.py
+++ dms/employee/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from employee import models
+
+
+@admin.register(models.Employee)
+class EmployeeModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'group')
+    list_filter = ('group', )
+    search_fields = ('id', 'name')
--- dms/employee/tests.py
+++ dms/employee/tests.py
@@ -1,5 +1,7 @@
+from django.contrib.admin.sites import AdminSite
 from django.test import TestCase
 
+from employee.admin import EmployeeModelAdmin
 from employee.models import Employee
 
 
@@ -16,10 +18,32 @@ class ModelTestCase(TestCase):
         self.assertEqual(employee.group, None)
 
 
-class BPMTestCase(TestCase):
+class BPMManagerTestCase(TestCase):
 
     def test_select_all_members_from_bpm(self):
         pass
 
     def test_execute(self):
         pass
+
+
+class ModelAdminTestCase(TestCase):
+
+    def setUp(self):
+        site = AdminSite()
+        self.page = EmployeeModelAdmin(Employee, site)
+
+    def test_list_display(self):
+        fields = ('id', 'name', 'email', 'group')
+        for field in fields:
+            self.assertIn(field, self.page.list_display)
+
+    def test_list_filter(self):
+        fields = ('group', )
+        for field in fields:
+            self.assertIn(field, self.page.list_filter)
+
+    def test_search_fields(self):
+        fields = ('id', 'name', )
+        for field in fields:
+            self.assertIn(field, self.page.search_fields)
