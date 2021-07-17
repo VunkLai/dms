2c48f76c3491dc42db229929e9d21ab68950ccc7
Vunk.Lai <vunk.lai@gmail.com>
2021-07-17 23:39:19 +0800

    [enhance] Add a cmd to rebuild the database

---
+++ dms/server/management/commands/server.py
@@ -0,0 +1,24 @@
+from typing import Any, Optional
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.management import call_command
+from django.core.management.base import BaseCommand, CommandParser
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument('action', choices=['build'])
+
+    def handle(self, *args: Any, **options: Any) -> Optional[str]:
+        if options['action'] == 'build':
+            if settings.DEBUG:
+                call_command('flush')
+                call_command('migrate')
+                user = User.objects.create_user('foo', 'foo.bar@gmail.com', 'bar')
+                user.is_superuser = True
+                user.is_staff = True
+                user.save()
+            return '[server] rebuild successfully'
+        return '[server] do nothing'
