63eb57c9455eb1ed0c2f85beefae2300e2aff3c1
Vunk.Lai <vunk.lai@gmail.com>
2021-07-17 23:21:03 +0800

    [enhance] Create Employee model and BPM manager

--- dms/employee/models.py
+++ dms/employee/models.py
@@ -1,1 +1,45 @@
+import typing
+
+from django.conf import settings
 from django.db import models
+
+import pymssql
+
+
+class DefaultManager(models.Manager):
+
+    pass
+
+
+class BusinessProcessManagement(models.Manager):
+
+    @staticmethod
+    def execute(sql: str) -> typing.Generator:
+        # pylint:disable=no-member
+        with pymssql.connect(**settings.MSSQL['BPM']) as conn:
+            with conn.cursor(as_dict=True) as cursor:
+                cursor.execute(sql)
+                yield from cursor
+
+    def select_all_members(self) -> typing.Generator:
+        yield from self.execute('''
+            SELECT AccountID AS id, DisplayName AS name, EMail AS email
+            FROM FSe7en_Org_MemberInfo
+        ''')
+
+
+class Employee(models.Model):
+
+    class Meta:
+        db_table = 'employee'
+        indexes = [
+            models.Index(fields=['group']),
+        ]
+
+    objects = DefaultManager()
+    from_bpm = BusinessProcessManagement()
+
+    id = models.CharField(max_length=30, primary_key=True)
+    name = models.CharField(max_length=79)
+    email = models.EmailField(**settings.NULLABLE)
+    group = models.CharField(max_length=1, **settings.NULLABLE)
--- dms/employee/tests.py
+++ dms/employee/tests.py
@@ -1,1 +1,25 @@
 from django.test import TestCase
+
+from employee.models import Employee
+
+
+class ModelTestCase(TestCase):
+
+    def tearDown(self) -> None:
+        Employee.objects.all().delete()
+
+    def test_nullable_fields(self):
+        employee = Employee.objects.create(id='Foo123', name='Bar')
+        self.assertEqual(employee.id, 'Foo123')
+        self.assertEqual(employee.name, 'Bar')
+        self.assertEqual(employee.email, None)
+        self.assertEqual(employee.group, None)
+
+
+class BPMTestCase(TestCase):
+
+    def test_select_all_members_from_bpm(self):
+        pass
+
+    def test_execute(self):
+        pass
