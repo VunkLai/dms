b6c98517811257c819c7bfeea0160ed37a899614
Vunk.Lai <vunk.lai@gmail.com>
2021-07-18 15:48:02 +0800

    [enhance] Send PSMC WIP report by PE cmd

--- dms/pe/management/commands/pe.py
+++ dms/pe/management/commands/pe.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from typing import Any
 
+from django.conf import settings
+from django.core.mail import EmailMessage
 from django.core.management.base import BaseCommand, CommandParser
 
 from pe import models
@@ -16,5 +18,19 @@ class Command(BaseCommand):
         tmp.mkdir(parents=True, exist_ok=True)
         excel = models.download_the_first_psmc_excel_of_today(folder=tmp)
         new_excel = models.extract_sheets_from_psmc_excel(excel)
+        email = EmailMessage(
+            subject='PSMC WIP report',
+            from_email=settings.EMAIL_HOST_USER,
+            to=settings.EMAIL_PE_PSMC_TO,
+            bcc=[settings.EMAIL_JOY],
+            # cc, reply_to,
+        )
+        email.attach_file(new_excel)
+        result = email.send(fail_silently=False)
 
-        return f'[download], {new_excel}'
+        data = {
+            'result': result,
+            'recipients': email.recipients(),
+            'attachments': [attachment[0] for attachment in email.attachments]
+        }
+        return f'[send] {data}'
