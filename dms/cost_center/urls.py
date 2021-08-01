50f7087c2d88d0458d853f5f47e6783e45d035ea
Vunk.Lai <vunk.lai@gmail.com>
2021-08-01 23:42:51 +0800

    [enhance] Create the views of Cost Center with a initial crud-table

--- dms/cost_center/urls.py
+++ dms/cost_center/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
+from cost_center import views
+
 urlpatterns = [
+    path('', views.home),
+    path('employees', views.employees),
 ]
--- dms/cost_center/views.py
+++ dms/cost_center/views.py
@@ -1,3 +1,39 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework import serializers
 
-# Create your views here.
+from cost_center.models import CostCenter
+from employee.models import Employee
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    centers = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
+
+    class Meta:
+        model = Employee
+        fields = ['id', 'name', 'centers']
+
+
+def home(request):
+    '''HTML'''
+    employees = Employee.objects.all()
+    content = {'employees': employees}
+    return render(request, 'cost_center/employees.html', content)
+
+
+def employees(request):
+    '''API'''
+    employees = Employee.objects.all()
+    serializer = EmployeeSerializer(employees, many=True)
+    return JsonResponse({'employees': serializer.data})
+
+
+def add(request, employee_id: str, cost_center: str):
+    '''API'''
+    employee = Employee.objects.get(id=employee_id)
+    CostCenter.objects.create(employee=employee, name=cost_center)
+    return JsonResponse({'status': 'ok'})
