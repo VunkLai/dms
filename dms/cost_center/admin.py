b5ba4545335cf273dbd14063be0e1c5ed1cdcc8f
Vunk.Lai <vunk.lai@gmail.com>
2021-08-03 22:05:52 +0800

    [feat] Create two admin views, CostCenter and UpdateRecords

--- dms/cost_center/admin.py
+++ dms/cost_center/admin.py
@@ -1,1 +1,18 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from cost_center import models
+
+
+@admin.register(models.CostCenter)
+class CostCenter(admin.ModelAdmin):
+
+    list_display = ('employee', 'name')
+    search_fields = ('name', )
+
+
+@admin.register(models.UpdateRecords)
+class UpdateRecords(admin.ModelAdmin):
+
+    list_display = ('created_at', 'user', 'employee', 'centers')
+    list_filter = ('created_at', 'user', 'employee')
+    search_fields = ('name', )
